411807de375ebe4fe5d8f7de03204caf13148b0d
weon_hyeok2 <kwh102455@gmail.com>
2024-05-26 20:07:40 +0900

    Add files via upload
    Baekjoon 10926 solved.

---
+++ BaekjoonJS/JSCode/problem10926.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("inputTxt/problem10926ex2.txt").toString().trim();
+const addStr = "??!";
+console.log(input+"??!");
+
+//case2
+//console.log(input+"??!");
