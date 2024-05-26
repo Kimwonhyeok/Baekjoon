3a5aa4abb6e33062dc6fe22eb5cc108019c72c5d
weon_hyeok2 <kwh102455@gmail.com>
2024-05-26 20:12:58 +0900

    Update (??!)10926.js

--- BaekjoonJS/JSCode/(??!)10926.js
+++ BaekjoonJS/JSCode/(??!)10926.js
@@ -5,3 +5,7 @@ console.log(input.concat(addStr));
 
 //case2
 //console.log(input+"??!");
+
+//trim() method 사용이유는 문자열 좌우 공백 제거를 위해서이다.
+https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+//referance
