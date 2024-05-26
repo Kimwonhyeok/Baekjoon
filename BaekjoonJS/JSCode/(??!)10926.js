8d7dbbe514478594404d0953e7b5e0fa44afb454
weon_hyeok2 <kwh102455@gmail.com>
2024-05-26 20:08:35 +0900

    Update and rename problem10926.js to (??!)10926.js

--- BaekjoonJS/JSCode/(??!)10926.js
+++ BaekjoonJS/JSCode/(??!)10926.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 let input = fs.readFileSync("inputTxt/problem10926ex2.txt").toString().trim();
 const addStr = "??!";
-console.log(input+"??!");
+console.log(input.concat(addStr));
 
 //case2
 //console.log(input+"??!");
